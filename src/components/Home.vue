f081fe5913fcbabdc257e0af9f643bf59dba1abc
Kenny Tang <kennyftang@gmail.com>
2017-10-28 18:15:19 -0500

    Adding components

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+<div id="home">
+  <div class="inputarea">
+    <input @click="showMap" class="zip" type="text" placeholder="Location">
+  </div>
+  <div class="about">
+    
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+  },
+  methods: {
+    showMap(e) {
+      console.log(e);
+    },
+  },
+};
+</script>
+
+<style>
+div#home, div.inputarea {
+  height: 100%;
+}
+div.about {
+  background-color: #f39;
+}
+div.inputarea {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+input.zip {
+  align-content: center;
+  font-size: 36pt;
+  text-align: center;
+  height: 2em;
+  width: 20em;
+}
+</style>
---
+++ src/components/MapView.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'mapview',
+  data() {
+  },
+};
+</script>
+
+<style>
+
+</style>
